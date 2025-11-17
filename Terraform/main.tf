bda4d197fe94c7f18a8bdbf476ac1f8e638460d2
Adeoye Emmanuel Eniola <150087990+DOTWEB2020@users.noreply.github.com>
2025-11-17 08:21:57 +0100

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,131 @@
+# terraform/main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 🔼 MUST COME FIRST: Define random_id before using it
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+# S3 Bucket for file uploads (NOW it can use random_id)
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "serverless-data-pipeline-${random_id.suffix.hex}"
+}
+
+# ... rest of your code remains the same ...
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.data_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+# DynamoDB Table
+resource "aws_dynamodb_table" "processed_data" {
+  name         = "ProcessedData"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "file_id"
+
+  attribute {
+    name = "file_id"
+    type = "S"
+  }
+}
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_s3_dynamodb" {
+  name = "lambda_s3_dynamodb_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectMetadata"
+        ]
+        Resource = "${aws_s3_bucket.data_bucket.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem"
+        ]
+        Resource = aws_dynamodb_table.processed_data.arn
+      }
+    ]
+  })
+}
+
+# Lambda Function
+resource "aws_lambda_function" "processor" {
+  filename      = "lambda_function.zip"
+  function_name = "DataProcessor"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.processed_data.name
+    }
+  }
+}
+
+# Lambda Permission for S3
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.processor.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.data_bucket.arn
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.data_bucket.bucket
+}
+
+output "dynamodb_table" {
+  value = aws_dynamodb_table.processed_data.name
+}
